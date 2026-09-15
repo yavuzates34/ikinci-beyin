d164c7eaf8a3a62d3101836f36ead45915525b74
Yavuz <yavuz.theprotagonist@gmail.com>
2026-09-16 02:53:52 +0300

    Derleme 2026-09-16

---
+++ araclar/derle-gece.cmd
@@ -0,0 +1,5 @@
+@echo off
+rem Gece calisan derleyici - Windows Gorev Zamanlayici bunu cagirir.
+rem Kayit: notlar/ikinci-beyin-mimarisi.md
+cd /d "C:\Users\Anj\Desktop\desktop\playground"
+python araclar\derle.py >> derleme\derleyici.log 2>&1
